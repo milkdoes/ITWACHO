05bc392f08dda10e00f84d7e397b50e093d53a0a
Salcedo Jose <josemasal@gmail.com>
2016-09-22 01:06:32 -0700

    2016-09-22

--- Web/JavaScript/Funciones.js
+++ Web/JavaScript/Funciones.js
@@ -1,4 +1,16 @@
+// Cargar Jquery local si fuera de linea.
 if (!window.jQuery) { document.write('<script src="JavaScript/jquery.min.js"></script>'); }
+
+// Cargar hoja de estilo si esta fuera de linea.
+/*
+(function($) {
+    $(function() {
+        if ($('body').css('font-size') !== '15px') {
+            $('head').prepend('<link rel="stylesheet" href="Partes/w3.css">');
+        }
+    });
+})(window.jQuery);
+*/
 
 function w3_open() {
     document.getElementById("mySidenav").style.display = "block";
