b3e4ee5880938f3a2ad0aa0f8cf8354cfe006d78
Salcedo Jose <josemasal@gmail.com>
2016-11-14 00:14:30 -0800

    2016-11-14

--- Web/js/Funciones.js
+++ Web/js/Funciones.js
@@ -8,17 +8,17 @@
     });
 })(window.jQuery);
 */
-function w3_open() {
-    document.getElementById("mySidenav").style.display = "block";
-    document.getElementById("myOverlay").style.display = "block";
-}
-
-function w3_close() {
-    document.getElementById("mySidenav").style.display = "none";
-    document.getElementById("myOverlay").style.display = "none";
-}
-
 $(document).ready(function() {
+    function w3_open() {
+        document.getElementById("mySidenav").style.display = "block";
+        document.getElementById("myOverlay").style.display = "block";
+    }
+
+    function w3_close() {
+        document.getElementById("mySidenav").style.display = "none";
+        document.getElementById("myOverlay").style.display = "none";
+    }
+
     $('#Navegacion').load('/ITWACHO/Web/Partes/Navegacion.html');
 });
 
